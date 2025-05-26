eb04c68ec1acd621a0afada2c2d86aeafa14d0ed
janko-julianna <janko.julianna@students.jedlik.eu>
2025-05-26 13:36:33 +0200

    adatbazis

---
+++ vet_clinic.appointments.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE vet_clinic.appointments (
+  id INT(11) NOT NULL,
+  pet_name VARCHAR(255) DEFAULT NULL,
+  owner_name VARCHAR(255) DEFAULT NULL,
+  appointment_date DATE DEFAULT NULL,
+  appointment_time TIME DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_general_ci;
